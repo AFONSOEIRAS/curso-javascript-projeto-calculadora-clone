7fe35728f68d0c66b4a14b5c9b9540f84dc78e62
AFONSOEIRAS <afonsoeiras26@gmail.com>
2022-03-28 00:32:08 -0300

    adicionando valores aos id display, data e hora

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,3 +1,34 @@
 class CalcController {
     
+    constructor(){
+
+        this._displayCalc = "0";
+        this._currentDate;
+        this.initialize();
+    }
+
+    initialize(){
+        let displayCalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+        displayCalcEl.innerHTML = "4567"
+        dateEl.innerHTML = "28/03/2022";
+        timeEl.innerHTML = "00:00";
+    }
+
+    get displayCalc(){
+        return this._displayCalc;
+    }
+
+    set displayCalc(valor){
+         this._displayCalc = valor;
+    }
+
+    get currentDate(){
+        return this.currentDate;
+    }
+
+    set dataAtual(valor){
+         this._dataAtual = valor;
+    }
 }
